88895ea786c24ee67d86b857610631a59a2d1cd1
roshith93 <r.roshith@gmail.com>
2019-07-15 12:24:08 +0530

    Initial Blood
    Installed axios and material ui. Also created an Navbar

---
+++ src/components/AppBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+
+ const  NavBar = () => {
+  return (
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton edge="start" color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit">
+            Pixabay Image Search
+          </Typography>
+        </Toolbar>
+      </AppBar>
+  );
+}
+
+export default NavBar
